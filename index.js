2b5cfb4ad3407cb8394139b2ce71f8c67c174093
Magda0006 <lenamagdamaria@gmail.com>
2025-01-20 16:36:17 +0000

    added js file + search engine basic function

---
+++ index.js
@@ -0,0 +1,8 @@
+function useSubmittedData(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+let searchEngineElemet = document.querySelector("#search-from");
+searchEngineElemet.addEventListener("submit", useSubmittedData);
